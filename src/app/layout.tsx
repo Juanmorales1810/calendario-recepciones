395e4c568da4f4f404d35160030d1ddc154662fb
Juan Morales <85465719+Juanmorales1810@users.noreply.github.com>
2026-01-16 16:50:07 -0300

    Agregar ThemeProvider y mejorar el diseño del layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 import { Toaster } from '@/components/ui/sonner';
 import { AuthProvider } from '@/components/auth';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -27,14 +28,68 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="es">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <NuqsAdapter>
-                    <AuthProvider>
-                        {children}
-                        <Toaster />
-                    </AuthProvider>
-                </NuqsAdapter>
+        <html lang="es" suppressHydrationWarning>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} relative min-h-screen w-full antialiased`}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange>
+                    <div
+                        className="absolute inset-0 -z-10 dark:opacity-40"
+                        style={{
+                            backgroundImage: `
+                            linear-gradient(to right, #e7e5e4 1px, transparent 1px),
+                            linear-gradient(to bottom, #e7e5e4 1px, transparent 1px)
+                        `,
+                            backgroundSize: '20px 20px',
+                            backgroundPosition: '0 0, 0 0',
+                            maskImage: `
+                            repeating-linear-gradient(
+                                to right,
+                                black 0px,
+                                black 3px,
+                                transparent 3px,
+                                transparent 8px
+                                ),
+                                repeating-linear-gradient(
+                                to bottom,
+                                black 0px,
+                                black 3px,
+                                transparent 3px,
+                                transparent 8px
+                                ),
+                                radial-gradient(ellipse 100% 80% at 50% 100%, #000 50%, transparent 90%)
+                        `,
+                            WebkitMaskImage: `
+                            repeating-linear-gradient(
+                                        to right,
+                                        black 0px,
+                                        black 3px,
+                                        transparent 3px,
+                                        transparent 8px
+                                        ),
+                                        repeating-linear-gradient(
+                                        to bottom,
+                                        black 0px,
+                                        black 3px,
+                                        transparent 3px,
+                                        transparent 8px
+                                        ),
+                                        radial-gradient(ellipse 100% 80% at 50% 100%, #000 50%, transparent 90%)
+                                `,
+                            maskComposite: 'intersect',
+                            WebkitMaskComposite: 'source-in',
+                        }}
+                    />
+                    <NuqsAdapter>
+                        <AuthProvider>
+                            {children}
+                            <Toaster />
+                        </AuthProvider>
+                    </NuqsAdapter>
+                </ThemeProvider>
             </body>
         </html>
     );
